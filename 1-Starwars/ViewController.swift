d40ab57b64473b3e96b5cebef6f69e642aa1c10d
Sidney Mangoye <mangoyesidney@gmail.com>
2016-01-06 03:54:31 +0900

    Initial Commit

---
+++ 1-Starwars/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  1-Starwars
+//
+//  Created by Sidney Mangoye on 06/01/16.
+//  Copyright © 2016 Sidney Mangoye. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
